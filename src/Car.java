97a5c47660731e1dc41c4650e21046cb9dd47705
Artem Onyshchenko <artemon888.com@gmail.com>
2025-08-13 17:06:17 +0300

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    public static void drive() {
+        System.out.println("Drive");
+        System.out.println("True");
+    }
+
+    protected static String stop() {
+        return "Stop";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.println("Hello World!\n");
+//        Car.drive();
+        System.out.println(Car.stop());
+        int a = 1;
+        System.out.println(a);
+        */
+
+    }
+}
+
