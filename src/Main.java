f463069b3f675556a11e9f4920fc751357f66f29
Artem Onyshchenko <artemon888.com@gmail.com>
2025-08-13 17:31:07 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,21 @@ public class Main {
         System.out.println(a);
         */
 
+        // primitive types: byte, short, int, long, float, double, char, boolean
+
+        byte x = 127; // 1 byte
+        short y = 32767; // 2 bytes
+        int z = 123456; // 4 bytes
+        long w = 1234567890L; // 8 bytes
+        float q = 1.2f; // 4 bytes
+        double r = 1.2d; // 8 bytes
+        char t = 'a'; // 2 bytes
+        boolean u = false; // 1 bit
+        /// //////
+        Integer v = Integer.valueOf(3);
+        // Strings are objects that represent character strings.
+        String str = "Hello";
+
     }
 }
 
