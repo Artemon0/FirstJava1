46f1255a9667d395f722305e4ef04dbf9de3b9e2
Artem Onyshchenko <artemon888.com@gmail.com>
2025-08-18 17:26:53 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+//        var a = "Hello";
+//        System.out.println("Hello World!");
+//        System.out.println(a);
+
+//        var b = 4;
+//        int x = 3;
+//        int z = 2;
+//
+//        double pi = 3.14f;
+
+//        System.out.println(++b);
+//        System.out.println(--b);
+
+//        int y = z + ++x; // x + 1
+//        System.out.println(y);
+//
+        int count = 10;
+        count += 5; // count = count + 5
+        System.out.println(count);
+        count -= 5; // count = count - 5
+        System.out.println(count);
+
+
+    }
+}
